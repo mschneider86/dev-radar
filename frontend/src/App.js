ce1b8722c70248474be4a4986a62272b84b6cf84
Matheus Schneider <matheus.ssm@gmail.com>
2020-06-10 19:56:39 -0300

    Added initial structure of the frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,105 @@
+import React from "react";
+
+import "./global.css";
+import "./App.css";
+import "./Sidebar.css";
+import "./Main.css";
+
+function App() {
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars3.githubusercontent.com/u/26752887?s=460&u=fa07959ea62de89ac27111f2adc00c8dad21ebab&v=4"
+                alt=""
+              />
+              <div className="user-info">
+                <strong>Diego</strong>
+                <span>react js</span>
+              </div>
+            </header>
+            <p>to da rocket</p>
+            <a href="http://www.com">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars3.githubusercontent.com/u/26752887?s=460&u=fa07959ea62de89ac27111f2adc00c8dad21ebab&v=4"
+                alt=""
+              />
+              <div className="user-info">
+                <strong>Diego</strong>
+                <span>react js</span>
+              </div>
+            </header>
+            <p>to da rocket</p>
+            <a href="http://www.com">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars3.githubusercontent.com/u/26752887?s=460&u=fa07959ea62de89ac27111f2adc00c8dad21ebab&v=4"
+                alt=""
+              />
+              <div className="user-info">
+                <strong>Diego</strong>
+                <span>react js</span>
+              </div>
+            </header>
+            <p>to da rocket</p>
+            <a href="http://www.com">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars3.githubusercontent.com/u/26752887?s=460&u=fa07959ea62de89ac27111f2adc00c8dad21ebab&v=4"
+                alt=""
+              />
+              <div className="user-info">
+                <strong>Diego</strong>
+                <span>react js</span>
+              </div>
+            </header>
+            <p>to da rocket</p>
+            <a href="http://www.com">Acessar perfil no Github</a>
+          </li>
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
