81a8cde7149425dffd9c2c2a62f99a48ad6ca420
Matheus Schneider <matheus.ssm@gmail.com>
2020-06-11 14:19:41 -0300

    Added initial websocket structure

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
+
 const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect(
   'mongodb+srv://user_omnistack:omnistack@cluster0-alt5r.mongodb.net/devradar?retryWrites=true&w=majority',
@@ -17,4 +23,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
